fc0cee2dae52bc38ee9fa17414506f19ec40ce47
Yashas Kumar <yrkumarr@gmail.com>
2014-07-16 11:18:23 -0500

    add Makefile and edit build.c

--- stdIO Project Template/build.c
+++ stdIO Project Template/build.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
+#include <stddef.h>
 #include <dlfcn.h>
 
 int main() {
     void* lib = dlopen("IO.so", RTLD_LAZY);
     if (lib == NULL) {
-        printf("Library failed to load\n");
-    }
+        printf("Library failed to load!\n");
+    } else {
+		printf("Library loaded successfully!\n");	
+	}
 }
