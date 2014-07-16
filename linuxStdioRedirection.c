bb2f9b6b19078c5cf5daa16f3ea23cea2138256f
LabVIEW User <labview@lvsl64-6.3>
2014-07-16 10:44:51 -0500

    Implemented linuxStdioRedirection.c. Still need open modes from Adam.

--- linuxStdioRedirection.c
+++ linuxStdioRedirection.c
@@ -1,5 +1,8 @@
 #include <string.h>
 
+#define READ_ONLY  0 // TODO get from adam
+#define WRITE_ONLY 0 // TODO get from adam
+ 
 typedef struct {                /* File reference number information record. */
     PathRef path;               /* Path of file opened. */
     File fd;                    /* File descriptor. */
@@ -13,7 +16,23 @@ typedef struct {                /* File reference number information record. */
     int16 openMode;             /* Store the open mode to be used when re-opening the file
                                    with deny access. */
 } FileRefNumInfo, *FileRefNumInfoPtr;
-    FileRefNumInfo STDIN = {path = 0, (File)stdin}
 
-FileRefNumInfo stdIn(){
+FileRefNumInfo stdRef(char *ref){
+    // TODO add strict checking
+    switch ref[0]{
+        case 'i':
+            FileRefNumInfo STDIN = {path = 0, (File)stdin, recNum = 0, version = 0, openMode, READ_ONLY}
+            return STDIN;
+            break;
+        case 'e':
+            FileRefNumInfo STDERR = {path = 0, (File)stderr, recNum = 0, version = 0, openMode, WRITE_ONLY}
+            return STDERR;
+            break;
+        case 'o':
+            FileRefNumInfo STDOUT = {path = 0, (File)stdout, recNum = 0, version = 0, openMode, WRITE_ONLY}
+            return STDOUT;
+            break;
+        default:
+            return 0;
+    }
 }
