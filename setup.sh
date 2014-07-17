93d4298c6a628a63b7ae963f051dbfe2cc6e7070
Caleb Wehrle <clwehrle@iastate.edu>
2014-07-17 11:16:05 -0500

    Added distribution package and setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo "Copying files to templates directory..."
+tar xvf std\ IO\ Project\ Template.tar.gz -C /usr/local/natinst/LabVIEW-2014-64/ProjectTemplates/Source
+
+echo "Copying xml metadata..."
+cp NILV_stdIO_Template_Metadata.xml  /usr/local/natinst/LabVIEW-2014-64/ProjectTemplates/Metadata
+
